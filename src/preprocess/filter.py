0e660cb8c19e5830060cd55eaec763e2337f7f36
jayz <yujinzhu88@gmail.com>
2021-05-24 16:48:05 -0400

    Update filter.py

--- src/preprocess/filter.py
+++ src/preprocess/filter.py
@@ -94,16 +94,12 @@ def rename_col(df, package_for_df='spark'):
     return df 
    
 
-def select_col(df, package_for_df='spark'):
+def select_col(df):
     '''select columns: id_str, time, latidude, and longitude, accuracy
     '''
     
     col_select = ['time', 'latitude', 'longitude', 'id_str']
-    
-    if package_for_df == 'spark':
-        df_select = df.select(*col_select)
-    else:
-        df_select = df[col_select]
+    df_select = df.select(*col_select)
     
     return df_select
 
@@ -206,7 +202,7 @@ def process_traj_indiv(df, days_need_min=1, package_for_df='spark', is_save=Fals
     #     print('Using dask')
     #     id_uniq = df['id_str'].unique().compute()            
             
-    def retrive_data_indiv(id_indiv, i, days_need_min=1, is_save=False):
+    def retrieve_data_indiv(id_indiv, i, days_need_min=1, is_save=False):
         
         if ( (i>=1) and (i%100==0) ):
             print('\n ===== retrieving the data for {}-th individual among {} ====='. format(i, len(id_uniq)))
@@ -221,26 +217,25 @@ def process_traj_indiv(df, days_need_min=1, package_for_df='spark', is_save=Fals
                             time_timestamp) )
         
         date_uniq = list(set(time_str))  # unique dates
-        is_indiv_save = 0
+        # is_indiv_save = 0
         if len(date_uniq) >= days_need_min:
             if is_save:
                 save_df_indiv(df=df_of_indiv, id_indiv = id_indiv)               
-                is_indiv_save = 1
                 
-        return is_indiv_save, df_of_indiv
+        return df_of_indiv
 
     def create_empty_spark_df():
         # create empty pyspark df for stop points
         field_temp = [StructField("id_indiv", StringType(), True),
-                      StructField("label", StringType(), True),
+                      StructField("label", IntegerType(), True),
                       StructField("start", IntegerType(), True),
                       StructField("end", IntegerType(), True),
                       StructField("latitude", FloatType(), True),
                       StructField("longitude", FloatType(), True)]
         schema_temp = StructType(field_temp)
-        df_stoppint = sqlContext.createDataFrame(sc.sparkContext.emptyRDD(), schema_temp)
+        df = sqlContext.createDataFrame(sc.sparkContext.emptyRDD(), schema_temp)
         
-        return df_stoppint
+        return df
     
     def infer_indiv_stoppoint(df = df_of_indiv):
         '''
@@ -275,60 +270,69 @@ def process_traj_indiv(df, days_need_min=1, package_for_df='spark', is_save=Fals
         id_indiv = df.loc[0, 'id_str']
         
         # if np.any(np.isnan(np.vstack(df_temp[['latitude', 'longitude', 'time']].values)))==False:
-        traj_array =  np.array(df_of_indiv.na.drop(how="any").select('latitude','longitude','time').collect())
-        time_array = traj_array[:, 2]
-        coord_array = traj_array[:, :2]
+            # drop na values: df_of_indiv.na.drop(how="any"); np.any(np.isnan(traj_array))
+            # i =6; df_of_indiv = retrieve_data_indiv(id_uniq[i], i)
+        # sort by time to be used in infostop model
+        df_of_indiv = df_of_indiv.sort(df_of_indiv.time.asc())
+        
+        # convert to array
+        traj_all =  np.array(df_of_indiv.select('latitude','longitude','time').collect())
+        time_all = traj_all[:, 2]
+        coord_all = traj_all[:, :2]
             
-        r1 , r2 = 30, 30
-        min_staying_time, max_time_between = 600, 86400
+        # define model
+        # max distance between time-consecutive points to label them as stationary, and max distance between stationary points to form an edge.
+        r1 , r2 = 30, 30 
+        min_staying_time, max_time_between = 600, 86400  # in seconds
         model_infostop = infostop.Infostop(r1 =r1, r2 =r2,
                                            label_singleton=False,
                                            min_staying_time = min_staying_time,
                                            max_time_between = max_time_between,
                                            min_size = 2)
 
+        # infer stops
         try:
-            # what are labels: transition -1; if stops, then the stop id, such as 1, 2, 3,
-            labels = model_infostop.fit_predict(traj_array)
+            # labels are for stops: transition -1; positive integer indicates stop id, such as 1, 2, 3,
+            labels = model_infostop.fit_predict(traj_all)
             is_stop_found = True
         except:
+            print("\n ===== Oops! Failed to find stop point for individual: {} ===== ".format(id_indiv))
             is_stop_found = False       
     
-        # remove points with labels indicating transition stops
+        # get the coordinates for the identifid stop points
         # create empty pyspark df
-        df_stoppint = create_empty_spark_df()
+        df_at_stop = create_empty_spark_df()
 
-        if is_stop_found and ( np.max(labels)>1 ):
-            # create empty df with columns names
-            trajectory = infostop.postprocess.compute_intervals(labels, time_array)    
+        n_stop = np.max(labels)
+        if is_stop_found and (n_stop>=2):
+            # get stop id, t_start, t_end
+            traj_at_stop = infostop.postprocess.compute_intervals(labels, time_all)
 
-                       
-            # keep the time and coordinates for stop points only among time and coordinates;
+            # get coordinates for stop points only;
             # those for transition stops will be filtered out
-            time_from_trajectory = trajectory[:, 0][:, None]
-            idx_stoppoint = (time_array==time_from_trajectory).any(axis=-1)
-            time_keep = time_array[idx_stoppoint]
-            coord_keep = coord_array[idx_stoppoint, :]
-            traj_data_stoppoint = trajectory[idx_stoppoint, :]
+            time_at_stop_start = np.array(traj_at_stop)[:, 1]  #[:, None]
+            idx_at_stop = np.where( np.in1d(time_all, time_at_stop_start) )[0]  #.any(axis=-1)
+            coord_at_stop = coord_all[idx_at_stop, :]
 
-            # fill the respective columns
+            # fill the olumns
             col_names_temp = ['label', 'start', 'end']
             for i in range(len(col_names_temp)):
-                df_stoppint.withColumn(col_names_temp[i], traj_data_stoppoint[:, i])
-        
-            df_stoppint.withColumn("latitude", coord_keep[:, 0])
-            df_stoppint.withColumn("longitude", coord_keep[:, 1])
+                df_stoppint.withColumn(col_names_temp[i], np.array(traj_at_stop)[:, i].tolist())
             
-            df_stoppint.withColumn("id_indiv", lit(id_indiv)) # fill with the same value
-            # trajectory[i] = ['label', 'start', 'end']            
+            df_stoppint.withColumn("latitude", coord_at_stop[:, 0])
+            df_stoppint.withColumn("longitude", coord_at_stop[:, 1])
+            
+            df_stoppint.withColumn("id_indiv", lit(id_indiv)) # fill with the same id           
      
-        return df_stoppint
+        return df_at_stop
 
 
     n_indiv_temp = 5
     
+    # TODO: retrieve data and then infer stop points for each individual
+    # use reduce instead of a for loop
     # list of df that is generated by applying nested functions
-    infer_indiv_stoppoint(df = df_of_indiv); is_indiv_save, df_of_indiv = retrive_data_indiv(id_indiv, i)
+    infer_indiv_stoppoint(df = df_of_indiv); is_indiv_save, df_of_indiv = retrieve_data_indiv(id_indiv, i)
     
     is_indiv_save = list( map( loop_over_indiv, id_uniq[n_indiv_temp], list( range(len(id_uniq[:n_indiv_temp])) ) ) )
     # is_indiv_save = list( map( loop_over_indiv, id_uniq, list( range(len(id_uniq)) ) ) )  
@@ -348,6 +352,10 @@ def main(is_save=False, package_for_df='spark'):
     # os.chdir("C:/Users/Jinzh/OneDrive/GitHub/Scale_Mobility/src")
     # os.chdir("/home/jayz/Documents/GitHub/Scale_Mobility/src")
     
+    
+    # TODO:
+   # move the setup of pyspark here within the main function after finalizing the previous functions
+     
     path_data = '../data'
     location = 'Albany'
     date = '20200207'
